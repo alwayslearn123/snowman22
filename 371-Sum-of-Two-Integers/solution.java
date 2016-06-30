30261100f41b6a3fa047602dcbcbd90583ff0509
alwayslearn123 <hotpot@gmail.com>
2016-06-30 04:15:13 -0400

    [371. Sum of Two Integers][Compile Error]committed by LeetCode Extension

---
+++ 371-Sum-of-Two-Integers/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int getSum(int a, int b) {
+        	    while (y != 0)
+	    {
+	        // carry now contains common set bits of x and y
+	        int carry = x & y;  
+	 
+	        // Sum of bits of x and y where at least one of the bits is not set
+	        x = x ^ y; 
+	 
+	        // Carry is shifted by one so that adding it to x gives the required sum
+	        y = carry << 1;
+	    }
+	    return x;
+    }
+}
